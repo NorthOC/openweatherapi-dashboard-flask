f5688e90da912f562fe995a575c02d8fdef1ca05
Denis Lisunov <rev.denisas@gmail.com>
2022-03-21 11:09:36 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,redirect, render_template, url_for
+from markupsafe import escape
+
+app = Flask(__name__)
+
+#redirect root to /welcome
+@app.route("/")
+def redirect_page():
+    return redirect(url_for("welcome"))
+
+
+@app.route("/welcome/")
+def welcome():
+    return render_template("welcome.html")
+
+#return welcome home or welcome back using same template.
+@app.route("/welcome/home")
+def welcome_home():
+    return render_template("_welcome.html", value = "home")
+
+@app.route("/welcome/back")
+def welcome_back():
+    return render_template("_welcome.html", value = "back")
