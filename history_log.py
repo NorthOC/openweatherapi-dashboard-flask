72cfde2b27037b8467dafc9f3abb5ebb2bc1007e
Denis Lisunov <rev.denisas@gmail.com>
2022-04-15 09:23:45 +0300

    final commit

--- history_log.py
+++ history_log.py
@@ -3,6 +3,7 @@ import os
 
 def update_history(city):
 
+  # prevent refresh page spam
   with open('logs/history.log', 'rb') as f:
     try:  # catch OSError in case of a one line file 
         f.seek(-2, os.SEEK_END)
@@ -12,11 +13,14 @@ def update_history(city):
         f.seek(0)
     last_line = f.readline().decode()
 
+  # logs
   date = str(datetime.now())
   searched_for = city['city']
   temp = str(city['temp'])
   log_string = "[" + date + "] " + searched_for + " " + temp + "\n"
+
   if log_string.split(" ")[2] + temp != last_line.split(" ")[2] + temp:
+
     with open('logs/history.log', 'a', encoding='utf-8') as f:
       f.write(log_string)
 
